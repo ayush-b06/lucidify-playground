00cb8b8c95d25d29111c4a147c543e7db9d7a44e
ayush <ayushbhujle@wifi-10-45-92-240.wifi.berkeley.edu>
2026-03-12 05:14:29 -0700

    Remove Firebase Analytics to fix SSR window error

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,6 +19,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-const analytics = getAnalytics(app)
 
 export { app, db, auth, googleProvider };
